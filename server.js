f32a96435dad3c3c15391928c8cea724ebbd33b3
ritik999 <uritik85@gmail.com>
2021-09-29 14:57:33 +0530

    adding env port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express=require("express");
 const app=express();
 const ejs=require("ejs");
 
-const port=process.env.port || 3000;
+const port=process.env.PORT || 3000;
 
 // database modle import
 const connectDB=require("./config/db");
